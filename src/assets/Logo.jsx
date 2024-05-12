5b2d2d05d6f202711d9f78ddc8f6a4e87671cf4e
Dimitri Freitas <freitasdimitri7@gmail.com>
2024-05-12 13:20:02 -0300

    migration for viteJs framework has been completed with success | new logo implemented

---
+++ src/assets/Logo.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+
+export const Logo = () => {
+  return (
+    <svg
+      width="120"
+      height="93"
+      viewBox="0 0 120 93"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M28.094 66.4891L39.7998 49.6327L83.8137 49.6327L90.3689 44.9504L116.59 44.9504L106.289 61.8068L84.7501 61.8068L57.1244 61.8068L51.9738 66.4891L28.094 66.4891Z"
+        fill="white"
+        fill-opacity="0.1"
+      />
+      <path
+        d="M0 90.1906L11.7058 73.3343L55.7197 73.3343L62.275 68.6519L88.496 68.6519L78.1949 85.5083L56.6562 85.5083L29.0304 85.5083L23.8799 90.1906L0 90.1906Z"
+        fill="white"
+        fill-opacity="0.1"
+      />
+      <path
+        d="M94.1148 28.094L82.409 44.9503L38.3951 44.9504L31.8398 49.6327L5.61878 49.6327L15.9199 32.7763L37.4586 32.7763L65.0844 32.7763L70.2349 28.094L94.1148 28.094Z"
+        fill="white"
+        fill-opacity="0.1"
+      />
+      <path
+        d="M115.654 2.80948L103.948 19.6659L59.9339 19.6659L53.3786 24.3482L27.1576 24.3482L37.4587 7.49182L58.9974 7.49181L86.6232 7.49181L91.7737 2.80948L115.654 2.80948Z"
+        fill="white"
+        fill-opacity="0.1"
+      />
+      <path
+        d="M30.9033 69.2985L42.6091 52.4421L86.623 52.4421L93.1783 47.7597L119.399 47.7597L109.098 64.6161L87.5595 64.6161L59.9338 64.6161L54.7832 69.2985L30.9033 69.2985Z"
+        fill="url(#paint0_linear_109_3)"
+      />
+      <path
+        d="M2.80936 93L14.5152 76.1436L58.5291 76.1436L65.0843 71.4613L91.3054 71.4613L81.0042 88.3177L59.4655 88.3177L31.8398 88.3177L26.6892 93L2.80936 93Z"
+        fill="url(#paint1_linear_109_3)"
+      />
+      <path
+        d="M91.3054 25.2846L79.5996 42.141L35.5857 42.141L29.0305 46.8233L2.80943 46.8233L13.1105 29.9669L34.6493 29.9669L62.275 29.9669L67.4256 25.2846L91.3054 25.2846Z"
+        fill="url(#paint2_linear_109_3)"
+      />
+      <path
+        d="M112.844 2.25138e-06L101.138 16.8564L57.1244 16.8564L50.5691 21.5387L24.3481 21.5387L34.6492 4.68234L56.1879 4.68234L83.8137 4.68233L88.9642 3.13147e-06L112.844 2.25138e-06Z"
+        fill="url(#paint3_linear_109_3)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_109_3"
+          x1="30.9033"
+          y1="69.2985"
+          x2="118.931"
+          y2="47.7597"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#BDFF00" />
+          <stop offset="1" stop-color="#CBFF37" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_109_3"
+          x1="2.80936"
+          y1="93"
+          x2="90.8371"
+          y2="71.4613"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#BDFF00" />
+          <stop offset="1" stop-color="#CBFF37" />
+        </linearGradient>
+        <linearGradient
+          id="paint2_linear_109_3"
+          x1="91.3054"
+          y1="25.2846"
+          x2="3.27766"
+          y2="46.8233"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#BDFF00" />
+          <stop offset="1" stop-color="#CBFF37" />
+        </linearGradient>
+        <linearGradient
+          id="paint3_linear_109_3"
+          x1="112.844"
+          y1="0"
+          x2="24.8163"
+          y2="21.5387"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#BDFF00" />
+          <stop offset="1" stop-color="#CBFF37" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import { Logo } from "../assets/Logo";
+import { LogoHeader } from "../assets/LogoHeader";
 
 export const Header = () => {
   return (
     <header className="bg-gray-800 flex py-3 gap-8 items-center px-20">
-      <Link to="/">{/* <Logo /> */}</Link>
+      <Link to="/">
+        <LogoHeader />
+      </Link>
     </header>
   );
 };
