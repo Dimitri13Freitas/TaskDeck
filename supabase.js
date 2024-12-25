7ee0ad7145e0be60fbb0e2b404a839504d79fb63
Dimitri13Freitas <freitasdimitri7@gmail.com>
2024-12-25 19:55:39 -0300

    integra variaveis de ambiente ao projeto

--- supabase.js
+++ supabase.js
@@ -1,4 +1,9 @@
-import { supabase } from "./connection";
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  import.meta.env.VITE_PROJECT_URL,
+  import.meta.env.VITE_ANON_KEY,
+);
 
 export class EndPoints {
   login() {
